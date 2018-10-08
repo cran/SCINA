32b393dec261751b12d38c9a215fd38f7c29240a
Ze Zhang <Ze.Zhang@utsouthwestern.edu>
2018-10-08 10:00:10 +0000

    version 1.0.0

---
+++ R/EM_functions.R
@@ -0,0 +1,109 @@
+check.inputs=function(exp, signatures, max_iter, convergence_n, convergence_rate, sensitivity_cutoff, rm_overlap, log_file)
+{
+  # initialize parameters
+  quality=1
+  def_max_iter=1000
+  def_conv_n=10
+  def_conv_rate=0.99
+  def_dummycut=0.33
+  allgenes=row.names(exp)
+  # Check exp
+  if (any(is.na(exp))){
+    cat('NA exists in expression matrix.',file=log_file,append=T,sep='\n')
+    cat('\n',file=log_file,append=T)
+    quality=0
+  }
+  # Check signatures
+  if (any(is.na(signatures))){
+    cat('Null cell type signature genes.',file=log_file,append=T,sep='\n');quality=0
+    cat('\n',file=log_file,append=T)
+  }else{
+    #na.omit(NAs) in each signatures, remove unvalid genes (not in exp)
+    signatures=sapply(signatures,function(x) unique(x[(!is.na(x)) & (x %in% allgenes)]),simplify=F)
+    #remove duplicate genes
+    if(rm_overlap==1){
+      tmp=table(unlist(signatures))
+      signatures=sapply(signatures,function(x) x[x %in% names(tmp[tmp==1])],simplify=F)
+    }
+    #check if any genes have all 0 counts
+    signatures=sapply(signatures,function(x) x[apply(exp[x,,drop=F],1,sd)>0],simplify=F)
+  }
+  # clean other parameters
+  if (is.na(convergence_n)){
+    cat('Using convergence_n=default',file=log_file,append=T,sep='\n')
+    cat('\n',file=log_file,append=T)
+    convergence_n=def_conv_n
+  }
+  if (is.na(max_iter)){
+    cat('Using max_iter=default',file=log_file,append=T,sep='\n')
+    cat('\n',file=log_file,append=T)
+    max_iter=def_max_iter
+  }else{
+    if (max_iter<convergence_n){
+      cat('Using max_iter=default due to smaller than convergence_n.',file=log_file,append=T,sep='\n')
+      cat('\n',file=log_file,append=T)
+      max_iter=convergence_n # this theorectically, still doesn't ensure max_iter>convergence_n (Tao)
+    }
+  }
+  if (is.na(convergence_rate)){
+    cat('Using convergence_rate=default.',file=log_file,append=T,sep='\n')
+    cat('\n',file=log_file,append=T)
+    convergence_rate=def_conv_rate
+  }
+  if (is.na(sensitivity_cutoff)){
+    cat('Using sensitivity_cutoff=default.',file=log_file,append=T,sep='\n')
+    cat('\n',file=log_file,append=T)
+    sensitivity_cutoff=def_dummycut
+  }
+  # return cleaned data
+  return(list(qual=quality,sig=signatures,
+              para=c(max_iter,convergence_n,convergence_rate,sensitivity_cutoff)))
+}
+
+density_ratio=function(e,mu1,mu2,inverse_sigma1,inverse_sigma2){
+  tmp1=colSums((e-mu1)*(inverse_sigma1%*%(e-mu1)))
+  tmp2=colSums((e-mu2)*(inverse_sigma2%*%(e-mu2)))
+  tmp=exp(-1/2*(tmp1+log(1/det(inverse_sigma1))-tmp2-log(1/det(inverse_sigma2))))
+  tmp[tmp>1e200]=1e200
+  tmp[tmp<1e-200]=1e-200
+  return(tmp)
+}
+
+findgenesigs=function(batched_mat,cells,n,cutoff=1.5){
+  mean_cell=c()
+  for (cell in cells){
+    tmp=log(batched_mat[[cell]][,1:n]+1)
+    mean_cell=cbind(mean_cell,rowMeans(tmp,na.rm = T))
+  }
+  signatures=list()
+  for(i in 1:dim(mean_cell)[1]){
+    cell_max=which.max(mean_cell[i,])
+    if (mean_cell[i,cell_max,drop=F]>max(mean_cell[i,-cell_max,drop=F])+cutoff) {
+      signatures[[cells[cell_max]]]=c(signatures[[cells[cell_max]]],rownames(mean_cell)[i])
+    }
+  }
+  return(signatures)
+}
+
+plotheat.SCINA=function(exp,results,signatures){
+  col_row=topo.colors(length(signatures))
+  col_col=cm.colors(length(unique(results$cell_labels)))
+  exp2plot=exp[unlist(signatures),order(factor(results$cell_labels,levels=c(names(signatures),'unknown')),decreasing=F)]
+  exp2plot=as.matrix(exp2plot)
+  col_colside=col_col[as.factor(results$cell_labels[order(factor(results$cell_labels,levels=c(names(signatures),'unknown')),decreasing=F)])]
+  col_rowside=col_row[unlist(sapply(1:length(signatures),function(i) rep(i, length(signatures[[i]]))))]
+  par(mar=rep(1,4))
+  plot=heatmap.2(exp2plot,trace='none',col=c('white','mistyrose1','lightpink1','lightpink2','lightpink3','lightpink4'),Rowv=FALSE,Colv=FALSE,dendrogram='none',xlab='Cells',ylab='Genes',labCol=NA,ColSideColors=col_colside,RowSideColors=col_rowside,key = F,margins = c(6.5,6.5))
+  legend_text=c(paste('Gene identifiers',names(signatures),sep='_'),names(signatures),'unknown')
+  legend_cor=c(col_row,unique(col_colside))
+  legend('topright',legend=legend_text,fill=legend_cor,cex=0.5)
+  return(invisible(plot))
+}
+
+preprocess.signatures=function(file_path){
+  csv_signatures=read.csv(file_path,stringsAsFactors = F,header = T,fill = F,quote ='')
+  signatures=as.list(csv_signatures)
+  signatures=sapply(signatures,function(x) x[(!is.na(x)) & x!=""],simplify = F)
+  return(signatures)
+}
+
---
+++ R/EM_model.R
@@ -0,0 +1,126 @@
+SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.99,
+               sensitivity_cutoff=1,rm_overlap=1,allow_unknown=1,log_file='SCINA.log')
+  {
+  cat('Start running SCINA.',file=log_file,append=F)
+  cat('\n',file=log_file,append=T)
+  all_sig=unique(unlist(signatures))
+  invert_sigs=grep('^low_',all_sig,value=T)
+  if(!identical(invert_sigs, character(0))){
+    cat('Converting expression matrix for low_genes.',file=log_file,append=T)
+    cat('\n',file=log_file,append=T)
+    invert_sigs_2add=unlist(lapply(invert_sigs,function(x) strsplit(x,'_')[[1]][2]))
+    invert_sigs=invert_sigs[invert_sigs_2add%in%row.names(exp)]# To avoid users adding 'low_' to non-existing genes.
+    invert_sigs_2add=invert_sigs_2add[invert_sigs_2add%in%row.names(exp)]
+    sub_exp=-exp[invert_sigs_2add,,drop=F]
+    row.names(sub_exp)=invert_sigs
+    exp=rbind(exp,sub_exp)
+    rm(sub_exp,all_sig,invert_sigs,invert_sigs_2add)
+  }
+  quality=check.inputs(exp,signatures,max_iter,convergence_n,convergence_rate,sensitivity_cutoff,rm_overlap,log_file)
+  if(quality$qual==0){
+    cat('EXITING due to invalid parameters.',file=log_file,append=T)
+    cat('\n',file=log_file,append=T)
+    stop('SCINA stopped.')
+  }
+  signatures=quality$sig
+  max_iter=quality$para[1]
+  convergence_n=quality$para[2]
+  convergence_rate=quality$para[3]
+  sensitivity_cutoff=quality$para[4]
+  # initialize variables
+  exp=as.matrix(exp)
+  exp=exp[unlist(signatures),,drop=F]
+  labels=matrix(0,ncol=convergence_n, nrow=dim(exp)[2])
+  unsatisfied=1
+  if(allow_unknown==1){
+    tao=rep(1/(length(signatures)+1),length(signatures))
+  }else{tao=rep(1/(length(signatures)),length(signatures))}
+  theta=list()
+  for(i in 1:length(signatures)){
+    theta[[i]]=list()
+    theta[[i]]$mean=t(apply(exp[signatures[[i]],,drop=F],1,function(x) quantile(x,c(0.7,0.3))))
+    tmp=apply(exp[signatures[[i]],,drop=F],1,var)
+    theta[[i]]$sigma1=diag(tmp,ncol = length(tmp))
+    theta[[i]]$sigma2=theta[[i]]$sigma1
+  }
+  sigma_min=min(sapply(theta,function(x) min(c(diag(x$sigma1),diag(x$sigma2)))))/100
+  remove_times=0
+  # SCINA
+  while(unsatisfied==1)
+  {
+    prob_mat=matrix(tao,ncol=dim(exp)[2],nrow=length(tao))
+    row.names(prob_mat)=names(signatures)
+    iter=0
+    labels_i=1
+    remove_times=remove_times+1
+
+    while(iter<max_iter)
+    {
+      iter=iter+1
+      #2. E_step
+      for(i in 1:length(signatures))
+      {
+        theta[[i]]$inverse_sigma1=theta[[i]]$inverse_sigma2=chol2inv(chol(theta[[i]]$sigma1))
+      }
+      for (r in 1:dim(prob_mat)[1])
+      {
+        prob_mat[r,]=tao[r]*density_ratio(exp[signatures[[r]],,drop=F],theta[[r]]$mean[,1],
+                                          theta[[r]]$mean[,2],theta[[r]]$inverse_sigma1,theta[[r]]$inverse_sigma2)
+      }
+      prob_mat=t(t(prob_mat)/(1-sum(tao)+colSums(prob_mat)))
+      #3. M-step
+      tao=rowMeans(prob_mat)
+      for(i in 1:length(signatures)){
+        #update means
+        theta[[i]]$mean[,1]=(exp[signatures[[i]],]%*%prob_mat[i,])/sum(prob_mat[i,])
+        theta[[i]]$mean[,2]=(exp[signatures[[i]],]%*%(1-prob_mat[i,]))/sum(1-prob_mat[i,])
+        #remain u1>u2
+        keep=theta[[i]]$mean[,1]<=theta[[i]]$mean[,2]
+        if(any(keep)){
+          theta[[i]]$mean[keep,1]=rowMeans(exp[signatures[[i]][keep],,drop=F])
+          theta[[i]]$mean[keep,2]=theta[[i]]$mean[keep,1]
+        }
+        #update covars
+        tmp1=t((exp[signatures[[i]],,drop=F]-theta[[i]]$mean[,1])^2)
+        tmp2=t((exp[signatures[[i]],,drop=F]-theta[[i]]$mean[,2])^2)
+        diag(theta[[i]]$sigma1)=diag(theta[[i]]$sigma2)=
+          colSums(tmp1*prob_mat[i,]+tmp2*(1-prob_mat[i,]))/dim(prob_mat)[2]
+        diag(theta[[i]]$sigma1)[diag(theta[[i]]$sigma1)<sigma_min]=sigma_min
+        diag(theta[[i]]$sigma2)[diag(theta[[i]]$sigma2)<sigma_min]=sigma_min
+      }
+
+      labels[,labels_i]=apply(rbind(1-colSums(prob_mat),prob_mat),2,which.max)-1
+      if(mean(apply(labels,1,function(x) length(unique(x))==1))>=convergence_rate){
+        cat('Job finished successfully.',file=log_file,append=T)
+        cat('\n',file=log_file,append=T)
+        break
+      }
+      labels_i=labels_i+1
+      if(labels_i==convergence_n+1){labels_i=1}
+      if(iter==max_iter){
+        cat('Maximum iterations, breaking out.',file=log_file,append=T)
+        cat('\n',file=log_file,append=T)
+      }
+    }
+    colnames(prob_mat)=colnames(exp)
+    row.names(prob_mat)=names(signatures)
+    row.names(labels)=colnames(exp)
+    #4. test dummy signature lists
+    dummytest=sapply(1:length(signatures),function(i) mean(theta[[i]]$mean[,1]-theta[[i]]$mean[,2]==0))
+    if(all(dummytest<=sensitivity_cutoff))
+    {
+      unsatisfied=0
+    }else
+    {
+      rev=which(dummytest>sensitivity_cutoff);
+      cat(paste('Remove dummy signatures:',rev,sep=' '),file=log_file,append=T,sep='\n');
+      # note the changes below
+      signatures=signatures[-rev]
+      tmp=1-sum(tao)
+      tao=tao[-rev]
+      tao=tao/(tmp+sum(tao))
+      theta=theta[-rev]
+    }
+  }
+  return(list(cell_labels=c("unknown",names(signatures))[1+labels[,labels_i]],probabilities=prob_mat))
+}
